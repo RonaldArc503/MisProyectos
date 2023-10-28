5231b81d7599021c7d54f056e36446ac3070b57f
RonaldArc503 <122319950+RonaldArc503@users.noreply.github.com>
2023-10-28 15:10:28 -0600

    login a

--- Markeplace/Markeplace/Form1.Designer.cs
+++ Markeplace/Markeplace/Form1.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(26, 176, 140);
+            panel1.ForeColor = Color.White;
+            panel1.Location = new Point(-2, -3);
+            panel1.Margin = new Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(516, 640);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Palatino Linotype", 27.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(554, 125);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 50);
+            label1.TabIndex = 1;
+            label1.Text = "Login";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("SimSun-ExtB", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(579, 227);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 24);
+            label2.TabIndex = 2;
+            label2.Text = "Usuario:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("SimSun-ExtB", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(579, 254);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(409, 37);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("SimSun-ExtB", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(579, 348);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(409, 35);
+            textBox2.TabIndex = 4;
+            textBox2.Tag = "";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("SimSun-ExtB", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(579, 321);
+            label3.Name = "label3";
+            label3.Size = new Size(140, 24);
+            label3.TabIndex = 5;
+            label3.Text = "Contraseña:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Underline, GraphicsUnit.Point);
+            label4.ForeColor = Color.FromArgb(0, 0, 192);
+            label4.Location = new Point(913, 417);
+            label4.Name = "label4";
+            label4.Size = new Size(112, 17);
+            label4.TabIndex = 6;
+            label4.Text = "No tienes cuenta?";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1063, 633);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Controls.Add(label4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
     }
 }
