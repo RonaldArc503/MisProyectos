0f908d5ed724230a13f7bff5a7c2abc54d865f9b
RonaldArc503 <122319950+RonaldArc503@users.noreply.github.com>
2023-11-12 11:26:39 -0600

    admins t

--- Markeplace/Markeplace/Administradores.Designer.cs
+++ Markeplace/Markeplace/Administradores.Designer.cs
@@ -29,19 +29,250 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Administradores));
+            userbox = new TextBox();
+            clavebox = new TextBox();
+            lastnamebox = new TextBox();
+            emailbox = new TextBox();
+            namebox = new TextBox();
+            label1 = new Label();
+            dataGridView2 = new DataGridView();
+            button2 = new Button();
+            idbox = new TextBox();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
+            // 
+            // userbox
+            // 
+            userbox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            userbox.ForeColor = Color.Gray;
+            userbox.Location = new Point(336, 193);
+            userbox.Margin = new Padding(0);
+            userbox.Multiline = true;
+            userbox.Name = "userbox";
+            userbox.Size = new Size(278, 37);
+            userbox.TabIndex = 32;
+            userbox.Tag = "";
+            userbox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // clavebox
+            // 
+            clavebox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            clavebox.ForeColor = Color.Gray;
+            clavebox.Location = new Point(614, 193);
+            clavebox.Margin = new Padding(0);
+            clavebox.Multiline = true;
+            clavebox.Name = "clavebox";
+            clavebox.Size = new Size(523, 37);
+            clavebox.TabIndex = 31;
+            clavebox.Tag = "";
+            clavebox.TextAlign = HorizontalAlignment.Center;
+            clavebox.TextChanged += clavebox_TextChanged;
+            // 
+            // lastnamebox
+            // 
+            lastnamebox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lastnamebox.ForeColor = Color.Gray;
+            lastnamebox.Location = new Point(614, 119);
+            lastnamebox.Margin = new Padding(0);
+            lastnamebox.Multiline = true;
+            lastnamebox.Name = "lastnamebox";
+            lastnamebox.Size = new Size(187, 37);
+            lastnamebox.TabIndex = 30;
+            lastnamebox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // emailbox
+            // 
+            emailbox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            emailbox.ForeColor = Color.Gray;
+            emailbox.Location = new Point(779, 119);
+            emailbox.Margin = new Padding(0);
+            emailbox.Multiline = true;
+            emailbox.Name = "emailbox";
+            emailbox.Size = new Size(358, 37);
+            emailbox.TabIndex = 29;
+            emailbox.Tag = "";
+            emailbox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // namebox
+            // 
+            namebox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            namebox.ForeColor = Color.Gray;
+            namebox.Location = new Point(427, 119);
+            namebox.Margin = new Padding(0);
+            namebox.Multiline = true;
+            namebox.Name = "namebox";
+            namebox.Size = new Size(187, 37);
+            namebox.TabIndex = 28;
+            namebox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Palatino Linotype", 27.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(425, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(308, 50);
+            label1.TabIndex = 27;
+            label1.Text = "Administradores";
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(336, 392);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.ReadOnly = true;
+            dataGridView2.RowTemplate.Height = 25;
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.Size = new Size(872, 150);
+            dataGridView2.TabIndex = 33;
+            dataGridView2.CellClick += dataGridView2_CellClick;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(22, 42);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 40);
+            button2.TabIndex = 34;
+            button2.Text = "Ver datos";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += llenardatos_Click;
+            // 
+            // idbox
+            // 
+            idbox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            idbox.ForeColor = Color.Gray;
+            idbox.Location = new Point(336, 119);
+            idbox.Margin = new Padding(0);
+            idbox.Multiline = true;
+            idbox.Name = "idbox";
+            idbox.Size = new Size(91, 37);
+            idbox.TabIndex = 35;
+            idbox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(22, 88);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 39);
+            button3.TabIndex = 36;
+            button3.Text = "Actualizar";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += actualizar_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.ForeColor = Color.Black;
+            textBox1.Location = new Point(425, 82);
+            textBox1.Margin = new Padding(0);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(189, 37);
+            textBox1.TabIndex = 37;
+            textBox1.Text = "Nombre";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox2.ForeColor = Color.Black;
+            textBox2.Location = new Point(614, 82);
+            textBox2.Margin = new Padding(0);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(187, 37);
+            textBox2.TabIndex = 38;
+            textBox2.Text = "Apellido";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox3.ForeColor = Color.Black;
+            textBox3.Location = new Point(801, 82);
+            textBox3.Margin = new Padding(0);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(336, 37);
+            textBox3.TabIndex = 39;
+            textBox3.Text = "Correo";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox4.ForeColor = Color.Black;
+            textBox4.Location = new Point(336, 156);
+            textBox4.Margin = new Padding(0);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(278, 37);
+            textBox4.TabIndex = 40;
+            textBox4.Text = "Usuario";
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox5.ForeColor = Color.Black;
+            textBox5.Location = new Point(614, 156);
+            textBox5.Margin = new Padding(0);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(523, 37);
+            textBox5.TabIndex = 41;
+            textBox5.Text = "Clave";
+            textBox5.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox6.ForeColor = Color.Black;
+            textBox6.Location = new Point(336, 82);
+            textBox6.Margin = new Padding(0);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(91, 37);
+            textBox6.TabIndex = 42;
+            textBox6.Text = "ID";
+            textBox6.TextAlign = HorizontalAlignment.Center;
             // 
             // Administradores
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1159, 540);
+            ClientSize = new Size(1275, 540);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(idbox);
+            Controls.Add(button2);
+            Controls.Add(dataGridView2);
+            Controls.Add(userbox);
+            Controls.Add(clavebox);
+            Controls.Add(lastnamebox);
+            Controls.Add(emailbox);
+            Controls.Add(namebox);
+            Controls.Add(label1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Administradores";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Administradores";
             Load += Administradores_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -56,5 +287,21 @@
         private System.CodeDom.CodeTypeReference usertable;
         private System.CodeDom.CodeTypeReference calvetable;
         private System.CodeDom.CodeTypeReference borntable;
+        private TextBox userbox;
+        private TextBox clavebox;
+        private TextBox lastnamebox;
+        private TextBox emailbox;
+        private TextBox namebox;
+        private Label label1;
+        private DataGridView dataGridView2;
+        private Button button2;
+        private TextBox idbox;
+        private Button button3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
     }
 }
--- Markeplace/Markeplace/Administradores.cs
+++ Markeplace/Markeplace/Administradores.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,7 @@ namespace Markeplace
             InitializeComponent();
 
         }
+        SqlConnection conexion = new SqlConnection("Server=DESKTOP-LALT64T; database=Markeplace; integrated security= true ");
 
 
         private void Administradores_Load(object sender, EventArgs e)
@@ -25,6 +27,174 @@ namespace Markeplace
             frm.Close();
         }
 
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            idbox.Text = dataGridView2.SelectedCells[0].Value.ToString();
+            namebox.Text = dataGridView2.SelectedCells[1].Value.ToString();
+            lastnamebox.Text = dataGridView2.SelectedCells[2].Value.ToString();
+            emailbox.Text = dataGridView2.SelectedCells[3].Value.ToString();
+            userbox.Text = dataGridView2.SelectedCells[4].Value.ToString();
+            clavebox.Text = dataGridView2.SelectedCells[5].Value.ToString();
+            //  datebbox.Text = dataGridView2.SelectedCells[6].Value.ToString();
 
+
+        }
+
+        private void llenar_tabla()
+        {
+
+            string consulta = "select*from Usuarios";
+            SqlDataAdapter adaptador = new SqlDataAdapter(consulta, conexion);
+            DataTable dt = new DataTable();
+            adaptador.Fill(dt);
+            dataGridView2.DataSource = dt;
+
+        }
+
+        private void actualizar_Click(object sender, EventArgs e)
+        {
+
+            conexion.Open();
+            string consulta = "UPDATE Usuarios SET Nombre = @Nombre, Apellido = @Apellido, Correo = @Correo, Usuario = @Usuario, Clave = @Clave WHERE id = @ID";
+            SqlCommand comando = new SqlCommand(consulta, conexion);
+            comando.Parameters.AddWithValue("@ID", idbox.Text);
+            comando.Parameters.AddWithValue("@Nombre", namebox.Text);
+            comando.Parameters.AddWithValue("@Apellido", lastnamebox.Text);
+            comando.Parameters.AddWithValue("@Correo", emailbox.Text);
+            comando.Parameters.AddWithValue("@Usuario", userbox.Text);
+            comando.Parameters.AddWithValue("@Clave", clavebox.Text);
+
+            int cant;
+            cant = comando.ExecuteNonQuery();
+
+            if (cant > 0)
+            {
+                MessageBox.Show("Datos actualizados");
+            }
+            llenar_tabla();
+            conexion.Close();
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // los efectos 
+
+        private void Nombreusuario_Enter(object sender, EventArgs e)
+        {
+            if (namebox.Text == "Nombre")
+            {
+                namebox.Text = "";
+                namebox.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void Nombreusuario_Leave(object sender, EventArgs e)
+        {
+            if (namebox.Text == "")
+            {
+                namebox.Text = "Nombre";
+                namebox.ForeColor = Color.Gray;
+            }
+        }
+
+        private void Apellidousuario_Enter(object sender, EventArgs e)
+        {
+            if (lastnamebox.Text == "Apellido")
+            {
+                lastnamebox.Text = "";
+                lastnamebox.ForeColor = Color.Black;
+            }
+        }
+
+        private void Apellidousuario_Leave(object sender, EventArgs e)
+        {
+            if (lastnamebox.Text == "")
+            {
+                lastnamebox.Text = "Apellido";
+                lastnamebox.ForeColor = Color.Gray;
+            }
+        }
+
+        private void Correo_Enter(object sender, EventArgs e)
+        {
+            if (emailbox.Text == "Correo electrónico")
+            {
+                emailbox.Text = "";
+                emailbox.ForeColor = Color.Black;
+            }
+        }
+
+        private void Correo_Leave(object sender, EventArgs e)
+        {
+            if (emailbox.Text == "")
+            {
+                emailbox.Text = "Correo electrónico";
+                emailbox.ForeColor = Color.Gray;
+            }
+        }
+
+        private void Contrasena_Enter(object sender, EventArgs e)
+        {
+            if (clavebox.Text == "Contraseña")
+            {
+                clavebox.Text = "";
+                clavebox.ForeColor = Color.Black;
+            }
+        }
+
+        private void Contrasena_Leave(object sender, EventArgs e)
+        {
+            if (clavebox.Text == "")
+            {
+                clavebox.Text = "Contraseña";
+                clavebox.ForeColor = Color.Gray;
+            }
+        }
+
+        private void User_Leave(object sender, EventArgs e)
+        {
+            if (userbox.Text == " ")
+            {
+                userbox.Text = "Usuario";
+                userbox.ForeColor = Color.Gray;
+            }
+        }
+
+        private void user_Enter(object sender, EventArgs e)
+        {
+            if (userbox.Text == "Usuario")
+            {
+                userbox.Text = " ";
+                userbox.ForeColor = Color.Black;
+            }
+        }
+
+        private void llenardatos_Click(object sender, EventArgs e)
+        {
+            llenar_tabla();
+        }
+
+        private void clavebox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
