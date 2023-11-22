f21ae463a64effb418a9ce9fca3a31ea4daadc57
RonaldArc503 <122319950+RonaldArc503@users.noreply.github.com>
2023-11-22 13:36:04 -0600

    datae

--- Markeplace/Markeplace/itemshop.Designer.cs
+++ Markeplace/Markeplace/itemshop.Designer.cs
@@ -29,63 +29,35 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(itemshop));
-            label1 = new Label();
             pictureBox1 = new PictureBox();
-            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(111, 244);
-            label1.Name = "label1";
-            label1.Size = new Size(183, 21);
-            label1.TabIndex = 0;
-            label1.Text = "RAMS DDR3 1600MHZ ";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(33, 28);
+            pictureBox1.Location = new Point(39, 22);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(213, 196);
+            pictureBox1.Size = new Size(177, 201);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 1;
+            pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(262, 3);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(40, 44);
-            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.TabIndex = 2;
-            pictureBox2.TabStop = false;
             // 
             // itemshop
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
-            Controls.Add(label1);
             Name = "itemshop";
-            Size = new Size(305, 283);
+            Size = new Size(263, 253);
             Load += itemshop_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-        private PictureBox pictureBox2;
-        public Label label1;
-        public PictureBox pictureBox1;
+
+        private PictureBox pictureBox1;
     }
 }
