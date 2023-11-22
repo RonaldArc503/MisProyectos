809edb737d2848c02e3b27b1445197fc99699e26
Erick <e.f.s.f.14@gmail.com>
2023-11-21 19:30:40 -0600

    Invento de programacion de edicion de perfil

--- Markeplace/Markeplace/Perfil.Designer.cs
+++ Markeplace/Markeplace/Perfil.Designer.cs
@@ -30,9 +30,32 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Perfil));
             perfilimage = new PictureBox();
-            button1 = new Button();
             label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            linkLabel1 = new LinkLabel();
+            lblemail = new Label();
+            lblname = new Label();
+            lbllastname = new Label();
+            lbluser = new Label();
+            panel1 = new Panel();
+            label15 = new Label();
+            txtconfirmarcontra = new TextBox();
+            txtcontra = new TextBox();
+            label13 = new Label();
+            label14 = new Label();
+            txtemail = new TextBox();
+            txtlastname = new TextBox();
+            txtfirstname = new TextBox();
+            txtusername = new TextBox();
+            label9 = new Label();
+            label10 = new Label();
+            label12 = new Label();
+            label11 = new Label();
+            label16 = new Label();
             ((System.ComponentModel.ISupportInitialize)perfilimage).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // perfilimage
@@ -40,44 +63,271 @@
             perfilimage.BackColor = Color.Transparent;
             perfilimage.BackgroundImageLayout = ImageLayout.None;
             perfilimage.Image = (Image)resources.GetObject("perfilimage.Image");
-            perfilimage.Location = new Point(12, 12);
+            perfilimage.Location = new Point(37, 28);
             perfilimage.Name = "perfilimage";
-            perfilimage.Size = new Size(56, 57);
+            perfilimage.Size = new Size(126, 135);
             perfilimage.SizeMode = PictureBoxSizeMode.StretchImage;
             perfilimage.TabIndex = 3;
             perfilimage.TabStop = false;
             // 
-            // button1
-            // 
-            button1.Location = new Point(101, 384);
-            button1.Name = "button1";
-            button1.Size = new Size(190, 47);
-            button1.TabIndex = 4;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(90, 29);
+            label1.Location = new Point(12, 166);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(66, 15);
             label1.TabIndex = 5;
-            label1.Text = "label1";
-         
+            label1.Text = "Username :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 254);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Lastname :";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(11, 212);
+            label3.Name = "label3";
+            label3.Size = new Size(65, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Firstname :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 305);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Email :";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(79, 370);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(64, 15);
+            linkLabel1.TabIndex = 9;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Edit profile";
+            // 
+            // lblemail
+            // 
+            lblemail.AutoSize = true;
+            lblemail.BackColor = Color.White;
+            lblemail.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblemail.ForeColor = Color.Gray;
+            lblemail.Location = new Point(12, 320);
+            lblemail.Name = "lblemail";
+            lblemail.Size = new Size(34, 13);
+            lblemail.TabIndex = 13;
+            lblemail.Text = "Email";
+            // 
+            // lblname
+            // 
+            lblname.AutoSize = true;
+            lblname.BackColor = Color.White;
+            lblname.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblname.ForeColor = Color.Gray;
+            lblname.Location = new Point(11, 227);
+            lblname.Name = "lblname";
+            lblname.Size = new Size(35, 13);
+            lblname.TabIndex = 12;
+            lblname.Text = "name";
+            // 
+            // lbllastname
+            // 
+            lbllastname.AutoSize = true;
+            lbllastname.BackColor = Color.White;
+            lbllastname.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbllastname.ForeColor = Color.Gray;
+            lbllastname.Location = new Point(12, 269);
+            lbllastname.Name = "lbllastname";
+            lbllastname.Size = new Size(55, 13);
+            lbllastname.TabIndex = 11;
+            lbllastname.Text = "Lastname";
+            // 
+            // lbluser
+            // 
+            lbluser.AutoSize = true;
+            lbluser.BackColor = Color.White;
+            lbluser.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbluser.ForeColor = Color.Gray;
+            lbluser.Location = new Point(12, 181);
+            lbluser.Name = "lbluser";
+            lbluser.Size = new Size(30, 13);
+            lbluser.TabIndex = 10;
+            lbluser.Text = "User";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(26, 176, 140);
+            panel1.Controls.Add(label15);
+            panel1.Controls.Add(txtconfirmarcontra);
+            panel1.Controls.Add(txtcontra);
+            panel1.Controls.Add(label13);
+            panel1.Controls.Add(label14);
+            panel1.Controls.Add(txtemail);
+            panel1.Controls.Add(txtlastname);
+            panel1.Controls.Add(txtfirstname);
+            panel1.Controls.Add(txtusername);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label10);
+            panel1.Controls.Add(label12);
+            panel1.Controls.Add(label11);
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(245, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(253, 433);
+            panel1.TabIndex = 14;
+            panel1.Visible = false;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label15.Location = new Point(37, 14);
+            label15.Name = "label15";
+            label15.Size = new Size(116, 25);
+            label15.TabIndex = 29;
+            label15.Text = "Edit my data";
+            // 
+            // txtconfirmarcontra
+            // 
+            txtconfirmarcontra.Location = new Point(36, 299);
+            txtconfirmarcontra.Name = "txtconfirmarcontra";
+            txtconfirmarcontra.Size = new Size(194, 23);
+            txtconfirmarcontra.TabIndex = 28;
+            // 
+            // txtcontra
+            // 
+            txtcontra.Location = new Point(36, 255);
+            txtcontra.Name = "txtcontra";
+            txtcontra.Size = new Size(194, 23);
+            txtcontra.TabIndex = 27;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(36, 281);
+            label13.Name = "label13";
+            label13.Size = new Size(110, 15);
+            label13.TabIndex = 26;
+            label13.Text = "Confirm password :";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(36, 237);
+            label14.Name = "label14";
+            label14.Size = new Size(63, 15);
+            label14.TabIndex = 25;
+            label14.Text = "Password :";
+            // 
+            // txtemail
+            // 
+            txtemail.Location = new Point(37, 210);
+            txtemail.Name = "txtemail";
+            txtemail.Size = new Size(193, 23);
+            txtemail.TabIndex = 24;
+            // 
+            // txtlastname
+            // 
+            txtlastname.Location = new Point(37, 166);
+            txtlastname.Name = "txtlastname";
+            txtlastname.Size = new Size(193, 23);
+            txtlastname.TabIndex = 23;
+            // 
+            // txtfirstname
+            // 
+            txtfirstname.Location = new Point(37, 122);
+            txtfirstname.Name = "txtfirstname";
+            txtfirstname.Size = new Size(193, 23);
+            txtfirstname.TabIndex = 22;
+            // 
+            // txtusername
+            // 
+            txtusername.Location = new Point(36, 79);
+            txtusername.Name = "txtusername";
+            txtusername.Size = new Size(194, 23);
+            txtusername.TabIndex = 21;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(36, 105);
+            label9.Name = "label9";
+            label9.Size = new Size(65, 15);
+            label9.TabIndex = 19;
+            label9.Text = "Firstname :";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(37, 192);
+            label10.Name = "label10";
+            label10.Size = new Size(42, 15);
+            label10.TabIndex = 20;
+            label10.Text = "Email :";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(36, 61);
+            label12.Name = "label12";
+            label12.Size = new Size(66, 15);
+            label12.TabIndex = 17;
+            label12.Text = "Username :";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(37, 148);
+            label11.Name = "label11";
+            label11.Size = new Size(64, 15);
+            label11.TabIndex = 18;
+            label11.Text = "Lastname :";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label16.Location = new Point(12, 0);
+            label16.Name = "label16";
+            label16.Size = new Size(98, 25);
+            label16.TabIndex = 30;
+            label16.Text = "My Profile";
             // 
             // Perfil
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(395, 492);
+            ClientSize = new Size(498, 433);
+            Controls.Add(label16);
+            Controls.Add(panel1);
+            Controls.Add(lblemail);
+            Controls.Add(lblname);
+            Controls.Add(lbllastname);
+            Controls.Add(lbluser);
+            Controls.Add(linkLabel1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(button1);
             Controls.Add(perfilimage);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Perfil";
             Text = "Perfil";
+            Load += Perfil_Load;
             ((System.ComponentModel.ISupportInitialize)perfilimage).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -85,7 +335,29 @@
         #endregion
 
         private PictureBox perfilimage;
-        private Button button1;
         private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private LinkLabel linkLabel1;
+        private Label lblemail;
+        private Label lblname;
+        private Label lbllastname;
+        private Label lbluser;
+        private Panel panel1;
+        private TextBox txtconfirmarcontra;
+        private TextBox txtcontra;
+        private Label label13;
+        private Label label14;
+        private TextBox txtemail;
+        private TextBox txtlastname;
+        private TextBox txtfirstname;
+        private TextBox txtusername;
+        private Label label9;
+        private Label label10;
+        private Label label12;
+        private Label label11;
+        private Label label15;
+        private Label label16;
     }
 }
--- Markeplace/Markeplace/Perfil.cs
+++ Markeplace/Markeplace/Perfil.cs
@@ -1,6 +1,15 @@
-﻿using System;
+﻿using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
+using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace Markeplace
 {
@@ -11,10 +20,68 @@ namespace Markeplace
             InitializeComponent();
         }
 
-      
         public void MostrarNombreApellido(string nombre, string apellido)
         {
-         //   label1.Text = $"Nombre: {nombre} {apellido}";
+            //   label1.Text = $"Nombre: {nombre} {apellido}";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Perfil_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void loadUserData()
+        {
+            try
+            {
+                string connectionString = "Server=DESKTOP-LALT64T; database=Markeplace; integrated security= true ";
+                string usuario = lbluser.Text;
+                string nombre = lblname.Text;
+                string apellido = lbllastname.Text;
+                string correo = lblemail.Text;
+                string user = txtusername.Text;
+                string nome = txtfirstname.Text;
+                string lastname = txtlastname.Text;
+                string email = txtemail.Text;
+
+
+                using (SqlConnection conexion = new SqlConnection(connectionString))
+                {
+                    conexion.Open();
+
+                    string consultaUsuario = "SELECT COUNT(*) FROM Usuarios WHERE Usuario = @Usuario, @Name = @Name, @Lastname = @Lastname, @Correo = @Correo";
+                    using (SqlCommand comando = new SqlCommand(consultaUsuario, conexion))
+                    {
+                        comando.Parameters.AddWithValue("@Usuario", usuario);
+                        comando.Parameters.AddWithValue("@Name", nombre);
+                        comando.Parameters.AddWithValue("@Lastname", apellido);
+                        comando.Parameters.AddWithValue("@Correo", correo);
+
+                        int resultadoUsuario = (int)comando.ExecuteScalar();
+                    }
+
+
+                }
+            }
+            catch (SqlException)
+            {
+              MessageBox.Show("error vuelva a intentar");
+            }
+
+
         }
     }
 }
+
+
+
+
